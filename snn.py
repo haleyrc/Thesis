0917119479c360b93ba69a63a12aa5eff5c23d36
Ryan Haley <saro.samurai@gmail.com>
2013-09-25 12:29:14 -0400

    Flesh out neuron and spike detector. Add entity list and basic time keeping.

--- snn.py
+++ snn.py
@@ -1,10 +1,32 @@
-class IAFNeuron( object ):
+from Queue import PriorityQueue
+
+class Clocked( object ):
 
   def __init__( self ):
-    pass
+    self.time = 0
+
+  def tick( self ):
+    self.time += 1
+
+class IAFNeuron( Clocked ):
+
+  next_id = 0
+
+  def __init__( self ):
+    super( IAFNeuron, self ).__init__()
+    self.targets = []
+    self.node_id = IAFNeuron.next_id
+    IAFNeuron.next_id += 1
 
   def __repr__( self ):
-    pass
+    return str( self.__class__ )
+
+  def connect( self, dest ):
+    self.targets.append( dest )
+
+  def spike( self ):
+    for target in self.targets:
+      target.spike( self.node_id )
 
 class ACGenerator( object ):
 
@@ -30,10 +52,37 @@ class Voltmeter( object ):
   def __repr__( self ):
     pass
 
-class SpikeDetector( object ):
+class SpikeDetector( Clocked ):
 
   def __init__( self ):
-    pass
+    self.spike_stream = PriorityQueue()
+    super( SpikeDetector, self ).__init__()
 
   def __repr__( self ):
-    pass
+    return str( self.__class__ )
+
+  def spike( self, source_id ):
+    # First parameter is place holder for tick time
+    self.spike_stream.put( (0, source_id ) )
+
+  def tick( self ):
+    self.time += 1
+
+class EntityList( object ):
+
+  def __init__( self ):
+    self.entity_list = []
+
+  def __len__( self ):
+    return len( self.entity_list )
+
+  def add( self, args ):
+    if not isinstance( args, list ):
+      args = [args]
+    
+    for arg in args:
+      self.entity_list.append( arg )
+
+  def tick( self ):
+    for entity in self.entity_list:
+      entity.tick()
--- test_neuron.py
+++ test_neuron.py
@@ -1,6 +1,78 @@
 from snn import *
 
 # Pointless test to check nose functionality
-def test_should_create_neuron_instance():
-  neuron = IAFNeuron()
-  assert( isinstance( neuron, IAFNeuron ) )
+class TestNeuron( object ):
+
+  def setup( self ):
+    self.neuron = IAFNeuron()
+    self.spike  = SpikeDetector()
+
+    self.neuron.connect( self.spike )
+
+  def test_should_create_neuron_instance( self ):
+    assert( isinstance( self.neuron, IAFNeuron ) )
+
+  def test_neuron_should_have_id( self ):
+    assert( self.neuron.node_id is not None )
+
+  def test_neuron_should_increment_id( self ):
+    next_neuron = IAFNeuron()
+    assert( next_neuron.node_id == self.neuron.node_id + 1 )
+
+  def test_neuron_should_add_connection( self ):
+    assert( len( self.neuron.targets ) == 1 )
+    assert( isinstance( self.neuron.targets[0], SpikeDetector ) )
+
+  def test_neuron_should_propagate_spikes( self ):
+    self.neuron.spike()
+    assert( self.spike.spike_stream.qsize() == 1 )
+
+class TestSpike( object ):
+
+  def setup( self ):
+    self.neuron = IAFNeuron()
+    self.spike  = SpikeDetector()
+
+    self.neuron.connect( self.spike )
+
+  def test_spike_detector_stream_should_have_correct_source_id( self ):
+    self.neuron.spike()
+    assert( self.neuron.node_id == self.spike.spike_stream.get()[1] )
+
+class TestClocked( object ):
+
+  def setup( self ):
+    self.neuron = IAFNeuron()
+
+  def test_should_start_at_time_zero( self ):
+    assert( self.neuron.time == 0 )
+
+  def test_tick_should_increment_time( self ):
+    self.neuron.tick()
+    assert( self.neuron.time == 1 )
+
+class TestEntityList( object ):
+
+  def setup( self ):
+    self.entities = EntityList()
+    self.neuron   = IAFNeuron()
+    self.spike    = SpikeDetector()
+
+    self.neuron.connect( self.spike )
+
+  def test_add_should_take_one_argument( self ):
+    self.entities.add( self.neuron )
+
+  def test_add_should_add_an_entity( self ):
+    self.entities.add( self.neuron )
+    assert( len( self.entities ) == 1 )
+
+  def test_add_should_take_multiple_arguments( self ):
+    self.entities.add( [self.neuron, self.spike ] )
+    assert( len( self.entities ) == 2 )
+
+  def test_tick_should_update_all_entities( self ):
+    self.entities.add( [self.neuron, self.spike ] )
+    self.entities.tick()
+    assert( self.neuron.time == 1 )
+    assert( self.spike.time  == 1 )
