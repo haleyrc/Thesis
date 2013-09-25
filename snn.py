61ef2921b4ecf9f6a20d963bfd3607cef5678bd9
Ryan Haley <saro.samurai@gmail.com>
2013-09-25 10:50:27 -0400

    Minor name changes.

--- snn.py
+++ snn.py
@@ -1,4 +1,4 @@
-class Neuron( object ):
+class IAFNeuron( object ):
 
   def __init__( self ):
     pass
--- test_neuron.py
+++ test_neuron.py
@@ -1,6 +1,6 @@
-from neuron import *
+from snn import *
 
 # Pointless test to check nose functionality
 def test_should_create_neuron_instance():
-  neuron = Neuron()
-  assert( isinstance( neuron, Neuron ) )
+  neuron = IAFNeuron()
+  assert( isinstance( neuron, IAFNeuron ) )
